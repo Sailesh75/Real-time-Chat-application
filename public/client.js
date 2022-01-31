e5b9512c08069b23965173326f813e52a45453f2
Sailesh Kafle <saileshkafle47@gmail.com>
2022-01-31 22:21:28 +0545

    initial commit

---
+++ public/client.js
@@ -0,0 +1,55 @@
+const socket = io();  //calling io
+let clientname;
+let textarea = document.querySelector('#textarea');
+let messageArea = document.querySelector('.message__area');
+
+do {
+    clientname = prompt('Please enter your name:')
+} while(!clientname)
+
+textarea.addEventListener('keyup', (e) => {
+    if(e.key === 'Enter') {
+        sendMessage(e.target.value)
+    }
+})
+
+function sendMessage(message) {
+    let msg = {
+        user: clientname,
+        message: message.trim()
+    }
+    // Append 
+    appendMessage(msg, 'outgoing')
+    textarea.value = ''
+    scrollToBottom()
+  
+    // Send to server 
+    socket.emit('message', msg)
+
+}
+
+function appendMessage(msg, type) {
+    let mainDiv = document.createElement('div')
+    let className = type
+    mainDiv.classList.add(className, 'message')
+
+    let markup = `
+        <h4>${msg.user}</h4>
+        <p>${msg.message}</p>
+    `
+    mainDiv.innerHTML = markup
+    messageArea.appendChild(mainDiv)
+}
+
+// Recieve messages 
+socket.on('message', (msg) => {
+    appendMessage(msg, 'incoming')
+    scrollToBottom()
+})
+
+function scrollToBottom() {
+    messageArea.scrollTop = messageArea.scrollHeight
+}
+
+
+
