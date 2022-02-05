5e973f1251af6ff2a150486a03c5e5e52435ef06
Sailesh Kafle <saileshkafle47@gmail.com>
2022-02-06 00:19:57 +0545

    api for postman

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
-//creating a express server
-//chat app 
 const express = require('express');
 const app = express();
-
 const http = require('http').createServer(app);
+const io = require('socket.io')(http)
 
 const PORT = process.env.PORT || 3000;
  
@@ -12,18 +10,29 @@ http.listen(PORT, () => {
 })
 
 app.use(express.static(__dirname + '/public'))  //using express middleware
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
 
+app.post('/form',(req,res)=>{
+    const {clientname,message}=req.body;
+    let msg = {
+        user:clientname,message:message
+    }
+    io.emit("message",msg);
+    io.on("message",(msg)=>{
+        console.log(msg)
+    })
+    res.status(201).json({message:"Message SEND!!"})    
+})
+
 // Socket 
-const io = require('socket.io')(http)
 
 io.on('connection', (socket) => {
     console.log('Connected...')
     socket.on('message', (msg) => {
         socket.broadcast.emit('message', msg)
     })
-    
 })
